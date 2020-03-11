341f49b9a56dded8af3668926e2534b742fc7ce6
sanyaele <samuelanyaele@gmail.com>
2020-03-11 05:33:06 +0100

    multi auth

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 
+   
+
 @section('content')
+
 <div class="container">
+
     <div class="row justify-content-center">
+
         <div class="col-md-8">
+
             <div class="card">
+
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    You are a normal user
+
+                    You are Admin.
+
                 </div>
+
             </div>
+
         </div>
+
     </div>
+
 </div>
+
 @endsection
