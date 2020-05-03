aa676348b96d1b38b4a7407167ed37c1cf040886
sanyaele <samuelanyaele@gmail.com>
2020-05-03 07:40:29 +0100

    Laravel

--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -35,8 +35,35 @@ class ClaimController extends Controller
 	 */
 	public function store(Request $request)
 	{
-		//
+		//First Register User, then get his id
 
+
+		// Populate Claim
+		$claim = new Claim;
+		$claim->user_id = $request->name;
+		$claim->airline_id = $request->description;
+		$claim->departure_id = $request->departureId;
+		$claim->arrival_id = $request->destinationId;
+
+		// Add new fields
+		// $claim->connecting = $request->connectingFlight;
+		// $claim->connections = $request->connections; // Create this in Vue
+
+		$claim->dof = $request->description;
+		$claim->tof = "None";
+		$claim->complaint = $request->name;
+		$claim->complaint_duration = $request->description;
+		$claim->complaint_option = "None";
+
+		$claim->save();
+
+		//$claim = Claim::create($request->all());
+
+		// Process Form logic, call class
+		$process_claim = \App\Claims\ProcessClaim::process($claim->id);
+
+		// Redirect to user registration form
+		return redirect()->route('/claims/processed', ['claims_id' => $claim->id]);
 	}
 
 	/**
